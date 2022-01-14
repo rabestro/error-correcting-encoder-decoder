38cf55183babbd4638a36eaffaeb3264b0845b39
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2022-01-15 00:29:48 +0200

    Add BinPrinter

---
+++ src/main/java/lv/id/jc/correcter/printer/BinPrinter.java
@@ -0,0 +1,25 @@
+package lv.id.jc.correcter.printer;
+
+import java.util.StringJoiner;
+
+/**
+ * Prints binary representation of data
+ */
+public class BinPrinter implements Printer {
+
+    @Override
+    public String apply(byte[] data) {
+        var binView = new StringJoiner(" ");
+        for (var element : data) {
+            binView.add(byteToBinary(element));
+        }
+        return "bin view: " + binView;
+    }
+
+    private String byteToBinary(byte data) {
+        return String
+                .format("%8s", Integer.toBinaryString(data & 0xFF))
+                .replace(' ', '0');
+    }
+
+}
