4a0b354d896bcd1c1ce9a23510a76641a3ab3211
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2022-01-15 00:47:46 +0200

    Add HexPrinterSpec

---
+++ src/test/groovy/lv/id/jc/correcter/printer/HexPrinterSpec.groovy
@@ -0,0 +1,24 @@
+package lv.id.jc.correcter.printer
+
+import spock.lang.Specification
+
+class HexPrinterSpec extends Specification {
+    def "should print hex view of data"() {
+        given:
+        def printer = new HexPrinter()
+
+        expect:
+        printer.apply(data) == hexView
+
+        where:
+        text                             | hex
+        'Test'                           | '54 65 73 74'
+        'Eat more of these french buns!' | '45 61 74 20 6d 6f 72 65 20 6f 66 20 74 68 65 73 65 20 66 72 65 6e 63 68 20 62 75 6e 73 21'
+
+        and:
+        data = text as byte[]
+
+        and:
+        hexView = 'hex view: ' + hex.toUpperCase()
+    }
+}
