e6dacd194750b97e1ddf42e72ebbc81e1fb7e807
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2022-01-15 00:43:19 +0200

    Add TextPrinterSpec

---
+++ src/test/groovy/lv/id/jc/correcter/printer/TextPrinterSpec.groovy
@@ -0,0 +1,27 @@
+package lv.id.jc.correcter.printer
+
+import spock.lang.Rollup
+import spock.lang.Specification
+import spock.lang.Subject
+
+class TextPrinterSpec extends Specification {
+
+    @Rollup
+    def "should print text view of data"() {
+        given:
+        @Subject
+        def printer = new TextPrinter()
+
+        and:
+        def data = text.bytes
+
+        expect:
+        printer.apply(data) == expected
+
+        where:
+        text << ['Test', 'Eat more of these french buns!', '']
+
+        and:
+        expected = "text view: $text"
+    }
+}
