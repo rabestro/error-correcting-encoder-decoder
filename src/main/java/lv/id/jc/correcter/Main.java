323e5176e13af717106c43b1037fab1db8e3d05b
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2022-01-15 00:34:26 +0200

    Add Main

---
+++ src/main/java/lv/id/jc/correcter/Main.java
@@ -0,0 +1,14 @@
+package lv.id.jc.correcter;
+
+import lv.id.jc.correcter.config.AppConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public final class Main {
+    public static void main(String[] args) {
+        var appContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        var application = appContext.getBean("application", Application.class);
+
+        application.run();
+    }
+
+}
