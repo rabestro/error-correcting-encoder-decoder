faff8ddf320918199a9fae2ea1f476fa866646ed
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2022-01-15 23:53:01 +0200

    Update AppConfig

--- src/main/java/lv/id/jc/correcter/Transmitter.java
+++ src/main/java/lv/id/jc/correcter/Transmitter.java
@@ -1,40 +1,30 @@
 package lv.id.jc.correcter;
 
-import lombok.Setter;
 import lv.id.jc.correcter.coder.Coder;
-import lv.id.jc.correcter.printer.Printer;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
 
 import static java.lang.System.Logger.Level.WARNING;
 import static java.lang.System.out;
 
-@Setter
-public class Transmitter implements Runnable {
+public record Transmitter(Coder coder, DataInfo source, DataInfo target) implements Runnable {
     private static final System.Logger LOGGER = System.getLogger("Transmitter");
-
-    private String inFile;
-    private String outFile;
-    private Coder coder;
-    private List<Printer> inPrinters;
-    private List<Printer> outPrinters;
 
     @Override
     public void run() {
         try {
-            var data = Files.readAllBytes(Paths.get(inFile));
-            out.println(inFile);
-            inPrinters.forEach(printer -> out.println(printer.apply(data)));
+            var data = Files.readAllBytes(Paths.get(source.file()));
+            out.println(source.file());
+            source.printers().forEach(printer -> out.println(printer.apply(data)));
 
             var coded = coder.apply(data);
 
             out.println();
-            out.println(outFile);
-            outPrinters.forEach(printer -> out.println(printer.apply(coded)));
-            Files.write(Paths.get(outFile), coded);
+            out.println(target.file());
+            target.printers().forEach(printer -> out.println(printer.apply(coded)));
+            Files.write(Paths.get(target.file()), coded);
         } catch (IOException e) {
             LOGGER.log(WARNING, e);
         }
