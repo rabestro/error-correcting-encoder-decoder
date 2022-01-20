6d22d3731e5f3b09a762892105905aa6edf9588a
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2022-01-20 18:25:05 +0200

    Add module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * Error Correcting Encoder-Decoder
+ */
+module correcter.main {
+    exports lv.id.jc.correcter;
+    opens lv.id.jc.correcter.config to spring.core, spring.beans, spring.context;
+
+    requires spring.context;
+    requires spring.core;
+}
+
